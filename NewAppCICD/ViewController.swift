eba614cb66fe0a19e4ffd42035b498f8d4404035
nilu.walmik@tissatech.com <nilu.walmik@tissatech.com>
2024-03-14 16:09:51 +0530

    First commit

---
+++ NewAppCICD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewAppCICD
+//
+//  Created by Tissa Macbook on 12/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-NewAppCICD/Pods-NewAppCICD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewAppCICD : NSObject
+@end
+@implementation PodsDummy_Pods_NewAppCICD
+@end
